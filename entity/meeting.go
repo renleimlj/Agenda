5a5f4618a98af141c7e0acfc28dbe0447799b2ac
RaoYuxi <805459700@qq.com>
2017-10-31 17:20:41 +0800

    Update meeting.go

--- entity/meeting.go
+++ entity/meeting.go
@@ -9,100 +9,112 @@ import (
 )
 
 type Meeting struct {
-  pr []string
-  st string
-  et string
-  title string
+  Title string
+  Spon string
+  Pr []string  
+  Start string
+  End string 
 }
-func (meeting Meeting) init(pr []string, st string, et string, title string) {
-  meeting.pr = pr
-  meeting.st = st
-  meeting.et = et
-  meeting.title = title
+
+func (meeting Meeting) init(Pr []string, St string, Et string, Title string) {
+  meeting.Pr = Pr
+  meeting.Start = St
+  meeting.End = Et
+  meeting.Title = Title
 }
 
 func (meeting Meeting) CopyMeeting (ano_meeting Meeting) {
-  meeting.pr = ano_meeting.pr
-  meeting.st = ano_meeting.st
-  meeting.et = ano_meeting.et
-  meeting.title = ano_meeting.title
+  meeting.Pr = ano_meeting.Pr
+  meeting.Start = ano_meeting.Start
+  meeting.End = ano_meeting.End
+  meeting.Title = ano_meeting.Title
 }
 
 func (meeting Meeting) GetParticipator() []string {
-    return pr
+    return meeting.Pr
 }
 
 func (meeting Meeting) SetParticipator(prs []string) {
-  var length= len(prs)
+  var length = len(prs)
   for i := 0; i < length; i++ {
-    meeting.pr[i]= prs[i]
+    meeting.Pr[i]= prs[i]
   }
 }
 
 func (meeting Meeting) GetStartTime() string {
-  return meeting.st
+  return meeting.Start
 }
 
-func (meeting Meeting) SetStartTime() (st string) {
-  meeting.st = st
+func (meeting Meeting) SetStartTime() (St string) {
+  meeting.Start = St
 }
+
+func (meeting Meeting) GetSponsor() string{
+  return meeting.Spon
+} 
+
+func (meeting Meeting) SetSponsor(Sp string) {
+  meeting.Spon = Sp
+} 
 
 func (meeting Meeting) GetEndTime() string {
-  return meeting.et
+  return meeting.End
 }
 
-func (meeting Meeting) SetEndTime() (et string) {
-  meeting.et = t_endTime
+func (meeting Meeting) SetEndTime() (Et string) {
+  meeting.End = Et
 }
 
 func (meeting Meeting) GetTitle() string {
-  return meeting.title
+  return meeting.Title
 }
 
-func _meeting Meeting) SetTitle(title string) {
-  meeting.title = title
+func _meeting Meeting) SetTitle(Title string) {
+  meeting.Title = Title
 }
 
-func (meeting Meeting) IsParticipator(un string) bool {
+func (meeting Meeting) IsParticipator(Un string) bool {
   var i int
-  for i= 0; i< len(meeting.pr); i++ {
-    if strings.EqualFold(meeting.pr[i], un)== true {
+  for i= 0; i< len(meeting.Pr); i++ {
+    if strings.EqualFold(meeting.Pr[i], Un)== true {
         return true
     }
   }
   return false
 }
 
-func (meeting Meeting) DeleteParticipator(un string) {
+func (meeting Meeting) DeleteParticipator(Un string) {
   var i int
-  num := len(meeting.pr)
+  num := len(meeting.Pr)
   for i= 0; i< num; i++ {
-    if strings.EqualFold(meeting.pr[i], un) == true {
-      meeting.pr = append(meeting.pr[:i], meeting.pr[i+1:]...)
+    if strings.EqualFold(meeting.Pr[i], un) == true {
+      meeting.Pr = append(meeting.Pr[:i], meeting.Pr[i+1:]...)
       break
     }
   }
-  num = len(meeting.pr)
+  num = len(meeting.Pr)
   if num == 0 {
     //delete meeting
   }
 }
 
-func (meeting Meeting)  AddParticipator(un string) bool {
+func (meeting Meeting) AddParticipator(Un string) bool {
+  // if time not overlap
   var i int
   var flag bool
-  flag= true
-  num := len(meeting.pr)
+  flag = true
+  num := len(meeting.Pr)
   for i= 0; i< num; i++ {
-    if strings.EqualFold(meeting.pr[i], un) == true {
+    if strings.EqualFold(meeting.Pr[i], un) == true {
       flag = false
       fmt.Println("AddParticipator failed! The user has already in!")  
       return false
     }
   }
   if flag == true {
-    meeting.pr = append(meeting.pr, un)
+    meeting.Pr = append(meeting.Pr, un)
     return true
   }
   return false
 }
+
